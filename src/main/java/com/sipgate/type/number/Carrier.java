7f77d26b44a9ebdcc2c189485d2c3f495a73fe87
musil <musil@sipgate.de>
2016-09-20 13:25:15 +0200

    Adds carrier

---
+++ src/main/java/com/sipgate/type/number/Carrier.java
@@ -0,0 +1,54 @@
+package com.sipgate.type.number;
+
+public enum Carrier
+{
+	NQ("D146", "nmsd"),
+	UMBRA("D248", "umbrad"),
+	PURPUR("D218", "purpurd"),
+	UNKNOWN("", "");
+
+	private String id;
+	private String system;
+
+	private Carrier(String id, String system)
+	{
+		this.id = id;
+		this.system = system;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public String getSystem()
+	{
+		return system;
+	}
+
+	public static Carrier parseSystem(String carrier)
+	{
+		for (final Carrier currentCarrier : values())
+		{
+			if (currentCarrier.getSystem().equalsIgnoreCase(carrier))
+			{
+				return currentCarrier;
+			}
+		}
+
+		return UNKNOWN;
+	}
+
+	public static Carrier parseCarrierId(String carrier)
+	{
+		for (final Carrier currentCarrier : values())
+		{
+			if (currentCarrier.getId().equalsIgnoreCase(carrier))
+			{
+				return currentCarrier;
+			}
+		}
+
+		return UNKNOWN;
+	}
+}
