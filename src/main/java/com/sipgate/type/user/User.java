357b848ad17a5102ef9c07f5d336ca22551cc5cf
musil <musil@sipgate.de>
2016-09-20 13:34:42 +0200

    implements user

--- src/main/java/com/sipgate/type/user/User.java
+++ src/main/java/com/sipgate/type/user/User.java
@@ -1,6 +1,35 @@
 package com.sipgate.type.user;
 
+import static java.text.MessageFormat.format;
+
+/**
+ * Convenience class bundling {@link MasterSipid} and {@link Domain}
+ * as a lot of old daemons couple these values extremely strong.
+ */
 public class User
 {
+	private final MasterSipid masterSipid;
+	private final Domain domain;
 
+	public User(MasterSipid masterSipid, Domain domain)
+	{
+		this.masterSipid = masterSipid;
+		this.domain = domain;
+	}
+
+	public MasterSipid getMasterSipid()
+	{
+		return masterSipid;
+	}
+
+	public Domain getDomain()
+	{
+		return domain;
+	}
+
+	@Override
+	public String toString()
+	{
+		return format("{0} ({1})", masterSipid, domain);
+	}
 }
