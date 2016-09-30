f36c0c4ffe4dfbb391cb9197216e890c32f5f382
musil <musil@sipgate.de>
2016-09-30 12:10:41 +0200

    adds test

--- src/test/java/com/sipgate/type/extension/ExtensionTypeTest.java
+++ src/test/java/com/sipgate/type/extension/ExtensionTypeTest.java
@@ -16,4 +16,13 @@ public class ExtensionTypeTest
 			assertThat("Checking type mapping for type " + type + ":", type.buildExtension("1234567", "1").getClass().getSimpleName(), is(type.toString()));
 		}
 	}
+
+	@Test
+	private void testToString()
+	{
+		for (final ExtensionType type : ExtensionType.values())
+		{
+			assertThat("Checking toString for type " + type + ":", type.buildExtension("1234567", "1").toString(), is("1234567" + type.toString().toLowerCase() + "1"));
+		}
+	}
 }
