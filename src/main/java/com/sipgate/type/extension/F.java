891d6413686a97d50cfbfba5e9c40016ee98efda
musil <musil@sipgate.de>
2016-09-30 12:13:16 +0200

    fixes description

--- src/main/java/com/sipgate/type/extension/F.java
+++ src/main/java/com/sipgate/type/extension/F.java
@@ -4,7 +4,7 @@ import static com.sipgate.type.extension.ExtensionType.V;
 
 public final class F extends Extension
 {
-	static final String DESCRIPTION = "Webfax-Extension, in+ou";
+	static final String DESCRIPTION = "Webfax-Extension, in+out";
 
 	F(String masterSipid, String id)
 	{
