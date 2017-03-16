32e49a16f8dc5217667fcb65c7cb498e39bd19fa
Moritz Maisel <maisel@sipgate.de>
2017-03-16 14:44:17 +0100

    reformats MasterSipid.java according to coding conventions

--- src/main/java/com/sipgate/type/user/MasterSipid.java
+++ src/main/java/com/sipgate/type/user/MasterSipid.java
@@ -11,8 +11,8 @@ import java.util.Optional;
 import static java.text.MessageFormat.format;
 import static org.apache.commons.lang3.StringUtils.trimToEmpty;
 
-public class MasterSipid implements Serializable
-{
+public class MasterSipid implements Serializable {
+
 	/**
 	 * Convenient way to build a new MasterSipid instance.
 	 * The given input will be validated and result in an {@link IllegalArgumentException} if the check fails.
@@ -20,72 +20,59 @@ public class MasterSipid implements Serializable
 	 * MasterSipid is serializable, a json serializer/deserializer will be provided.
 	 *
 	 * @param masterSipid String representation of a mastersipid.
-	 *
-	 *  @throws IllegalArgumentException in case of any invalid input that doesn't
-	 *
 	 * @return An MasterSipid instance
+	 * @throws IllegalArgumentException in case of any invalid input that doesn't
 	 */
-	public static MasterSipid of(String masterSipid)
-	{
+	public static MasterSipid of(String masterSipid) {
 		final String input = trimToEmpty(masterSipid);
 
-		if (invalid(input))
-		{
+		if (invalid(input)) {
 			throw new IllegalArgumentException(format("Cannot validate {0} to a valid masterSipid", masterSipid));
 		}
 
 		return new MasterSipid(input);
 	}
 
-	public static Optional<MasterSipid> parse(String masterSipid)
-	{
+	public static Optional<MasterSipid> parse(String masterSipid) {
 		final String input = trimToEmpty(masterSipid);
 
-		if (invalid(input))
-		{
+		if (invalid(input)) {
 			return Optional.empty();
 		}
 
 		return Optional.of(new MasterSipid(input));
 	}
 
-	private static boolean invalid(String masterSipid)
-	{
-		return ! masterSipid.matches("^\\d{7}$");
+	private static boolean invalid(String masterSipid) {
+		return !masterSipid.matches("^\\d{7}$");
 	}
 
 	private final String masterSipid;
 
-	private MasterSipid(String key)
-	{
+	private MasterSipid(String key) {
 		this.masterSipid = key;
 	}
 
-	public String getKey()
-	{
+	public String getKey() {
 		return masterSipid;
 	}
 
-	public Extension deriveExtension(ExtensionType type, String id)
-	{
+	public Extension deriveExtension(ExtensionType type, String id) {
 		return Extension.build(this, type, id);
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return masterSipid;
 	}
 
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		return HashCodeBuilder.reflectionHashCode(this, false);
 	}
 
 	@Override
-	public boolean equals(Object that)
-	{
+	public boolean equals(Object that) {
 		return EqualsBuilder.reflectionEquals(this, that, false);
 	}
 }
