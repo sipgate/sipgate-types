58f3283c975f22b8be3b765e6f1ccca63e2c126b
Benjamin Kluck <kluck@sipgate.de>
2017-05-30 18:15:05 +0200

    Add SessionId

---
+++ src/main/java/com/sipgate/type/calls/SessionId.java
@@ -0,0 +1,89 @@
+package com.sipgate.type.calls;
+
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+public class SessionId {
+
+	static final byte[] key =
+		("feOcden_yetlenJoaccebkelNiecactartum*gensonEicykEcMec=da"
+			+ "nofyown8AvonedUgRovNugdegcockiejOt0ocBegrekmgav6leuzsi7"
+			+ "Wab5okEygEwQuitUtmymsiavEnsimIcmutIpivGawnOuAtlihontsta"
+			+ "DapgewIcByzajnovSibojmor4drefgaHebGeccedavLipgimOchAmWo"
+			+ "NifleedIgpictatnecbuevRysbaidNokPeetsyatedmothyejCedPes"
+			+ "CecwudpooghacVabyeryamLybOvnedByisokpokmynwinubnuetsilg"
+			+ "weadpidgugTeugutmegWimkejUgfapKeHyweskyujCulWuctIlkUcOr"
+			+ "AksavyiBlidenshOdbeikijopIcDincikqueracviedNiwacyapJiHo"
+			+ "pacnudyiedZytUfAvElAgFadvulPevaijlykEarj0swisicOgdaktof"
+			+ "CruguchEgdivAmduwybNoorceurdyeckEfGooHeivifOfdeenJododa"
+			+ "RottAtNiWothkefroachacInItVeHikGhawElWiKrardOgCikWozhai"
+			+ "TajEexyolnunCizfobrIfeeghodsinyaukTimgalUtyobEkyaylgOj4"
+			+ "plytyachujCumicekMarfIgEcyofAfIbfitcashekVajbykMotvevFi"
+			+ "FebtyrumObseetDitlinadsIrElyevMiUbdavOpsEcDeHebyavyicBu"
+			+ "VekFindOcheituvQuondimougjebMovticwityibOocOgUrbiaHiUls")
+			.getBytes(StandardCharsets.ISO_8859_1);
+
+	private final String id;
+	private final String owner;
+	private final String tos;
+
+	public SessionId(final String id, final String owner, final String tos) {
+		this.id = id;
+		this.owner = owner;
+		this.tos = tos;
+	}
+
+	public SessionId(final String encoded) throws Exception {
+		final String decoded = decode(encoded);
+		final String[] split = decoded.split("\\$");
+		this.id = split[2];
+		this.owner = split[3];
+		this.tos = split[1];
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public String getTos() {
+		return tos;
+	}
+
+	public String getEncoded() {
+		return encode(this.id, this.owner, this.tos);
+	}
+
+	private String encode(final String id, final String owner, final String tos) {
+		String combinedId =
+			Integer.toHexString(id.length() + owner.length() + tos.length() + 2) + "$" + tos + "$" + id
+				+ "$"
+				+ owner;
+
+		byte[] plain = combinedId.getBytes(StandardCharsets.ISO_8859_1);
+		byte[] encoded = new byte[plain.length];
+
+		for (int i = 0; i < plain.length; i++) {
+
+			encoded[i] = (byte) (plain[i] ^ key[i % key.length]);
+		}
+
+		return String.valueOf(Hex.encodeHex(encoded)).toUpperCase();
+	}
+
+	private String decode(final String encoded) throws DecoderException {
+		byte[] encodedBytes = Hex.decodeHex(encoded.toLowerCase().toCharArray());
+		byte[] decodedBytes = new byte[encodedBytes.length];
+
+		for (int i = 0; i < encodedBytes.length; i++) {
+
+			decodedBytes[i] = (byte) (encodedBytes[i] ^ key[i % key.length]);
+		}
+
+		return new String(decodedBytes, StandardCharsets.ISO_8859_1);
+	}
+}
---
+++ src/test/java/com/sipgate/type/user/calls/SessionIdTest.java
@@ -0,0 +1,41 @@
+package com.sipgate.type.user.calls;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.sipgate.type.calls.SessionId;
+import org.junit.Test;
+
+public class SessionIdTest {
+	private final String id = "click2api-0049163777-1168212e1-0.465734058138079-hasel";
+	private final String owner = "1168212w0";
+	private final String tos = "api-voice";
+	private final String encoded = "52046B02140C4329160C1709410D2606020851041202485C7E5D5C52575043564559445C1C5F575F410A5F68594D4D5D70547E5153085C505D57564E565A06593213034A5455635F605E443945";
+
+	@Test
+	public void encodeDecode() throws Exception {
+		SessionId fromDecoded = new SessionId(id, owner, tos);
+		SessionId fromEncoded = new SessionId(fromDecoded.getEncoded());
+
+		assertThat(fromEncoded.getId(), is(id));
+		assertThat(fromEncoded.getOwner(), is(owner));
+		assertThat(fromEncoded.getTos(), is(tos));
+	}
+
+	@Test
+	public void encodesToApiDCompatible() {
+		SessionId sessionId = new SessionId(id, owner, tos);
+		assertThat(
+			sessionId.getEncoded(),
+			is(encoded)
+		);
+	}
+
+	@Test
+	public void decodesToApiDCompatible() throws Exception {
+		SessionId sessionId = new SessionId(encoded);
+		assertThat(sessionId.getId(), is(id));
+		assertThat(sessionId.getOwner(), is(owner));
+		assertThat(sessionId.getTos(), is(tos));
+	}
+}
